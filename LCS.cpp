f933abdd353917de1374785df12761e1922d3f2e
Payton Redemer <paytonredemer@gmail.com>
2022-10-31 23:23:06 -0500

    everything but asserts

--- LCS.cpp
+++ LCS.cpp
@@ -18,30 +18,30 @@ int main()
     std::string x = "ACCGGTCGAGTGCGCGGAAGCCGGCCGAA";
     std::string y = "GTCGTTCGGAATGCCGTTGCTCTGTAAA";
 
-    x = "ATGCCA";
-    y = "TCACCA";
+    // Inserting space at beginning of each string to make algorithm easier to convert
+    x.insert(0," ");
+    y.insert(0," ");
 
     int m = x.length();
     int n = y.length();
 
-    // char b[m+1][n+1];
+
+    // create dynamic array b[m+1][n+1] to pass to function
     char **b = new char*[m+1];
     for(int i = 0; i <= m; i++)
         b[i] = new char[n+1];
 
+    // create b table and save length of LCS
     int length = lcs_length(x, y, m, n, b);
 
     std::cout << "The longest common subsequence is \"";
     print_lcs(b, x, m, n);
     std::cout << "\" and the length is " << length-1 << std::endl;
 
+    // Freeing memory
     for(int i = 0; i <= m; i++)
-    {
-        for(int j = 0; j <= n; j++)
-            std::cout << b[i][j] << " ";
-        std::cout << std::endl;
-    }
-    
+        delete[] b[i];
+    delete[] b;
 
     return 0;
 }
@@ -63,20 +63,21 @@ int lcs_length(std::string x, std::string y, int m, int n, char** b)
             if(x[i] == y[j])
             {
                 c[i][j] = c[i-1][j-1] + 1;
-                b[i][j] = 'D';
+                b[i][j] = 'D'; // Diagonal arrow
             }
             else if (c[i-1][j] >= c[i][j-1])
             {
                 c[i][j] = c[i-1][j];
-                b[i][j] = 'U';
+                b[i][j] = 'U'; // Up arrow
             }
             else
             {
                 c[i][j] = c[i][j-1];
-                b[i][j] = 'L';
+                b[i][j] = 'L'; // Left arrow
             }
         }
     }
+
     return c[m][n];
 }
 
@@ -85,13 +86,16 @@ void print_lcs(char** b, std::string x, int i, int j)
     if(i == 0 || j == 0)
         return;
     
+    // Diagonal arrow
     if(b[i][j] == 'D')
     {
         print_lcs(b, x, i-1, j-1);
         std::cout << x[i];
     }
+    // Up arrow
     else if(b[i][j] == 'U')
         print_lcs(b, x, i-1, j);
+    // Left arrow
     else
         print_lcs(b, x, i, j-1);
 }
